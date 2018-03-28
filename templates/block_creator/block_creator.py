0ef02b0744ffee3eeb84e2b6f99805aac5cb152c
Khaled Karam <khaledkbadr@outlook.com>
2018-03-28 22:28:12 +0200

    add walletpassphrase and monitor actions to block_creator
    Signed-off-by: Khaled Karam <khaledkbadr@outlook.com>

--- templates/block_creator/block_creator.py
+++ templates/block_creator/block_creator.py
@@ -1,9 +1,11 @@
 from js9 import j
 
 from zerorobot.template.base import TemplateBase
+from zerorobot.template.state import StateCheckError
+
 
 CONTAINER_TEMPLATE_UID = 'github.com/zero-os/0-templates/container/0.0.1'
-TFCHAIN_FLIST = 'https://hub.gig.tech/lee/ubuntu-16.04-tfchain-v0.1.0.flist'
+TFCHAIN_FLIST = 'https://hub.gig.tech/khaledkbadr/tfchain-ubuntu.flist'
 
 
 class BlockCreator(TemplateBase):
@@ -14,9 +16,16 @@ class BlockCreator(TemplateBase):
         super().__init__(name=name, guid=guid, data=data)
         self._tfchain_sal = None
 
+        self.recurring_action('_monitor', 300)  # every 300 seconds
+
+    def validate(self):
+        for param in ['walletPassphrase']:
+            if not self.data[param]:
+                raise ValueError("parameter '%s' not valid: %s" % (param, str(self.data[param])))
+
     @property
     def node_sal(self):
-        return j.clients.zero_os.sal.node_get(self.data['node'])
+        return j.clients.zero_os.sal.get_node(self.data['node'])
 
     @property
     def container_sal(self):
@@ -30,8 +39,9 @@ class BlockCreator(TemplateBase):
                 'container': self.container_sal,
                 'rpc_addr': '0.0.0.0:%s' % self.data['rpcPort'],
                 'api_addr': 'localhost:%s' % self.data['apiPort'],
+                'wallet_passphrase': self.data['walletPassphrase'],
             }
-            self._tfchain_sal = j.clients.zero_os.sal.tfchain_get(**kwargs)
+            self._tfchain_sal = j.clients.zero_os.sal.get_tfchain(**kwargs)
         return self._tfchain_sal
 
     def install(self):
@@ -45,7 +55,8 @@ class BlockCreator(TemplateBase):
         }
 
         self.data['container'] = 'container_%s' % self.name
-        container = self.api.services.create(CONTAINER_TEMPLATE_UID, self.data['container'], data=container_data)
+        container = self.api.services.create(
+            CONTAINER_TEMPLATE_UID, self.data['container'], data=container_data)
         container.schedule_action('install').wait()
 
         self.state.set('actions', 'install', 'ok')
@@ -57,9 +68,11 @@ class BlockCreator(TemplateBase):
         self.state.check('actions', 'install', 'ok')
 
         self.logger.info('Staring tfchaind {}'.format(self.name))
-        container = self.api.services.get(template_uid=CONTAINER_TEMPLATE_UID, name=self.data['container'])
+        container = self.api.services.get(
+            template_uid=CONTAINER_TEMPLATE_UID, name=self.data['container'])
         container.schedule_action('start').wait()
         self.tchain_sal.daemon.start()
+        self.state.set('status', 'running', 'ok')
 
         try:
             self.state.check('wallet', 'init', 'ok')
@@ -69,7 +82,6 @@ class BlockCreator(TemplateBase):
             self.tchain_sal.client.wallet_unlock()
 
         self.data['walletSeed'] = self.tchain_sal.client.recovery_seed
-        self.data['walletPassphrase'] = self.tchain_sal.client.wallet_password
         self.state.set('actions', 'start', 'ok')
         self.state.set('wallet', 'init', 'ok')
 
@@ -78,6 +90,18 @@ class BlockCreator(TemplateBase):
         stop tfchain daemon
         """
         self.state.check('actions', 'install', 'ok')
-        self.logger.info('Stopping rivine daemon {}'.format(self.name))
+        self.logger.info('Stopping tfchain daemon {}'.format(self.name))
         self.tchain_sal.daemon.stop()
         self.state.delete('actions', 'install')
+        self.state.delete('status', 'running')
+
+    def _monitor(self):
+        if not self.tchain_sal.daemon.is_running():
+            self.state.delete('status', 'running')
+            return
+
+        try:
+            self.state.check('status', 'running', 'ok')
+            self.start()
+        except StateCheckError:
+            pass
