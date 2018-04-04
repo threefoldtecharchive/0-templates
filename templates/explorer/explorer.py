a2976dcea5601781fce79f164cc2038185c519f6
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2018-04-04 14:19:14 +0200

    add explorer templates
    - also refactor part of block_creator to use latest js9 SAL code

--- templates/explorer/explorer.py
+++ templates/explorer/explorer.py
@@ -8,21 +8,21 @@ from zerorobot.service_collection import ServiceNotFoundError
 
 CONTAINER_TEMPLATE_UID = 'github.com/zero-os/0-templates/container/0.0.1'
 TFCHAIN_FLIST = 'https://hub.gig.tech/tfchain/ubuntu-16.04-tfchain-latest.flist'
+EXPLORER_FLIST = 'https://hub.gig.tech/tfchain/caddy-explorer-latest.flist'
 
 
-class BlockCreator(TemplateBase):
+class Explorer(TemplateBase):
     version = '0.0.1'
-    template_name = 'block_creator'
+    template_name = 'explorer'
 
     def __init__(self, name=None, guid=None, data=None):
         super().__init__(name=name, guid=guid, data=data)
         self._tfchain_sal = None
-
-        wallet_passphrase = self.data.get('walletPassphrase')
-        if not wallet_passphrase:
-            self.data['walletPassphrase'] = j.data.idgenerator.generateGUID()
-
         self.recurring_action('_monitor', 30)  # every 30 seconds
+
+    def validate(self):
+        if not self.data.get('domain'):
+            raise ValueError("domain need to be specified")
 
     @property
     def _node_sal(self):
@@ -37,25 +37,16 @@ class BlockCreator(TemplateBase):
         return "container-%s" % self.guid
 
     @property
-    def _daemon_sal(self):
+    def _explorer_sal(self):
         kwargs = {
             'name': self.name,
             'container': self._container_sal,
             'rpc_addr': '0.0.0.0:%s' % self.data['rpcPort'],
             'api_addr': 'localhost:%s' % self.data['apiPort'],
             'data_dir': '/mnt/data',
+            'domain': self.data['domain'],
         }
-        return j.clients.zero_os.sal.tfchain.daemon(**kwargs)
-
-    @property
-    def _client_sal(self):
-        kwargs = {
-            'name': self.name,
-            'container': self._container_sal,
-            'api_addr': 'localhost:%s' % self.data['apiPort'],
-            'wallet_passphrase': self.data['walletPassphrase'],
-        }
-        return j.clients.zero_os.sal.tfchain.client(**kwargs)
+        return j.clients.zero_os.sal.tfchain.explorer(**kwargs)
 
     def _get_container(self):
         sp = self._node_sal.storagepools.get('zos-cache')
@@ -64,31 +55,30 @@ class BlockCreator(TemplateBase):
         except ValueError:
             fs = sp.create(self.guid)
 
-        ports = ['%s:%s' % (self.data['rpcPort'], self.data['rpcPort'])]
-
         # prepare persistant volume to mount into the container
         node_fs = self._node_sal.client.filesystem
-        vol = os.path.join(fs.path, 'wallet')
+        vol = os.path.join(fs.path, 'explorer')
         node_fs.mkdir(vol)
-        mounts = [{
-            'source': vol,
-            'target': '/mnt/data'
-        }]
+        mounts = [
+            {'source': vol,
+             'target': '/mnt/data'},
+            {'source': EXPLORER_FLIST,
+             'target': '/mnt/explorer'}
+        ]
 
         container_data = {
             'flist': TFCHAIN_FLIST,
             'node': self.data['node'],
             'nics': [{'type': 'default'}],
             'mounts': mounts,
-            'ports': ports
+            'ports': ['80:80', '443:443'],
         }
 
         return self.api.services.find_or_create(CONTAINER_TEMPLATE_UID, self._container_name, data=container_data)
 
     def install(self):
         """
-        Creating tfchain container with the provided flist, and configure mounts for datadirs
-            'flist': TFCHAIN_FLIST,
+        Creating tfchain container with the provided flist, and configure mounts for data dirs
         """
         container = self._get_container()
         container.schedule_action('install').wait(die=True)
@@ -96,7 +86,7 @@ class BlockCreator(TemplateBase):
 
     def uninstall(self):
         """
-        Remove the persistent volume of the wallet, and delete the container
+        Remove the persistent volume of the explorer, and delete the container
         """
         try:
             self.stop()
@@ -118,41 +108,32 @@ class BlockCreator(TemplateBase):
 
     def start(self):
         """
-        start both tfchain daemon and client
+        start both tfchain explorer and caddy
         """
         self.state.check('actions', 'install', 'ok')
 
-        self.logger.info('Staring tfchaind {}'.format(self.name))
+        self.logger.info('Staring tfchaind %s', self.name)
         container = self._get_container()
         container.schedule_action('start').wait(die=True)
 
-        self.logger.info('Starting tfcaind %s' % self.name)
-        self._daemon_sal.start()
+        self.logger.info('Starting tfcaind and caddy (%s)', self.name)
+        self._explorer_sal.start()
         self.state.set('status', 'running', 'ok')
 
-        try:
-            self.state.check('wallet', 'init', 'ok')
-        except StateCheckError:
-            self.logger.info('initalizing wallet %s' % self.name)
-            time.sleep(2)  # seems to be need for the daemon to be ready to init the wallet
-            self._client_sal.wallet_init()
-            self.data['walletSeed'] = self._client_sal.recovery_seed
-
-        time.sleep(2)  # seems to be need for the daemon to be ready to unlock the wallet
-        self._client_sal.wallet_unlock()
-
         self._node_sal.client.nft.open_port(self.data['rpcPort'])
+        self._node_sal.client.nft.open_port(443)
+        self._node_sal.client.nft.open_port(80)
 
         self.state.set('actions', 'start', 'ok')
         self.state.set('wallet', 'init', 'ok')
 
     def stop(self):
         """
-        stop tfchain daemon
+        stop tfchain explorer and caddy
         """
-        self.logger.info('Stopping tfchain daemon {}'.format(self.name))
+        self.logger.info('Stopping tfchain explorer and caddy (%s)', self.name)
         try:
-            self._daemon_sal.stop()
+            self._explorer_sal.stop()
             # force stop container
             container = self.api.services.get(template_uid=CONTAINER_TEMPLATE_UID, name=self._container_name)
             container.schedule_action('stop').wait(die=True)
@@ -162,49 +143,41 @@ class BlockCreator(TemplateBase):
             pass
 
         self._node_sal.client.nft.drop_port(self.data['rpcPort'])
+        self._node_sal.client.nft.drop_port(443)
+        self._node_sal.client.nft.drop_port(80)
+
         self.state.delete('status', 'running')
         self.state.delete('actions', 'start')
 
     def upgrade(self):
-        """upgrade the container with an updated flist
+        """
+        upgrade the container with an updated flist
         this is done by stopping the container and respawn again with the updated flist
         """
-        # stop daemon
         self.stop()
-
-        # restart daemon in new container
         self.start()
-
-    def wallet_address(self):
-        """
-        load wallet address into the service's data
-        """
-        self.state.check('wallet', 'init', 'ok')
-
-        if not self.data.get('walletAddr'):
-            self.data['walletAddr'] = self._client_sal.wallet_address
-        return self.data['walletAddr']
-
-    def wallet_amount(self):
-        """
-        return the amount of token in the wallet
-        """
-        self.state.check('wallet', 'init', 'ok')
-        return self._client_sal.wallet_amount()
 
     def consensus_stat(self):
         """
         return information about the state of consensus
         """
         self.state.check('status', 'running', 'ok')
-        return self._client_sal.consensus_stat()
+        return self._explorer_sal.consensus_stat()
+
+    def gateway(self):
+        """
+        return information about the number of peers connected
+        e.g: {'Active peers': '0', 'Address': '96.182.165.25'}
+        """
+        self.state.check('status', 'running', 'ok')
+        return self._explorer_sal.consensus_stat()
 
     def _monitor(self):
         self.state.check('actions', 'install', 'ok')
         self.state.check('actions', 'start', 'ok')
 
         try:
-            if self._daemon_sal.is_running():
+            if self._explorer_sal.explorer.is_running():
                 self.state.set('status', 'running', 'ok')
                 return
         except LookupError:
