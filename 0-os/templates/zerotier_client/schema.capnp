fb3eb46a6176aa20f7a532acd739a1b148f001a6
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2018-08-01 15:45:21 +0200

    move all template into a file layout the robot understands

--- 0-os/templates/zerotier_client/schema.capnp
+++
@@ -1,6 +0,0 @@
-@0xa9de4f7dcccf86af;
-
-struct Schema {
-    token @0: Text;
-    # API token for zerotier
-}
--- 0-os/utils/flist.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-cd /0-templates
-tar -cpzf "/tmp/archives/0-templates.tar.gz" templates
