478e1e23d6ce304a723cd6fa6d397f69225cc906
Chris Van de Gejuchte <chris.vandegejuchte@gmail.com>
2018-08-31 15:12:25 +0200

    updates portal_connection template

--- templates/portal_connection/portal_connection.py
+++ templates/portal_connection/portal_connection.py
@@ -1,4 +1,7 @@
 import requests
+import os
+import socket
+import psutil
 from jumpscale import j
 from zerorobot.template.base import TemplateBase
 from zerorobot.server import auth
@@ -27,20 +30,42 @@ class PortalConnection(TemplateBase):
         """
         return j.clients.zos.get(NODE_CLIENT)
 
-    def install(self):
+    def install(self, username="", password=""):
+        auth_token = self._authenticate(username, password)
+
+        robot_ip, robot_port = self._get_listen_address()
+        cookies = {"beaker.session.id": auth_token}
         data = {
             'name': self._node_sal.name,
-            'url': self._node_sal,
+            'url': "http://{ip}:{port}".format(ip=robot_ip, port=robot_port),
             'godToken': auth.god_jwt.create()
         }
-        resp = requests.post(self.data['url'], json=data)
+        resp = requests.post("{base_url}/restmachine/zrobot/client/add".format(base_url=self.data['url']), json=data, cookies=cookies)
         resp.raise_for_status()
 
         self.state.set('actions', 'install', 'ok')
 
-    def uninstall(self):
+    def _get_listen_address(self):
+        r_pid = os.getpid()
+        for c in psutil.net_connections('inet4'):
+            if c.pid == r_pid and c.type == socket.SOCK_STREAM and c.status == psutil.CONN_LISTEN:
+                return c.laddr
+        raise RuntimeError("Could not determine listening address")
+
+    def uninstall(self, username, password):
+        auth_token = self._authenticate(username, password)
+        cookies = {"beaker.session.id": auth_token}
+
         data = {'name': self._node_sal.name}
-        resp = requests.post(self.data['url'], json=data)
+        resp = requests.post("{base_url}/restmachine/zrobot/client/delete".format(base_url=self.data['url']), json=data, cookies=cookies)
         resp.raise_for_status()
 
         self.state.delete('actions', 'install')
+
+    def _authenticate(self, username, password):
+        resp = requests.post(
+            "{base_url}/restmachine/system/usermanager/authenticate".format(base_url=self.data['url']),
+            params={"name":username, "secret":password})
+        resp.raise_for_status()
+        auth_token = resp.json()
+        return auth_token
