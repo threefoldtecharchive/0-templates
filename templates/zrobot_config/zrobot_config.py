c019e688bc3dee8b20bdaf2ce9e935b52a9741cf
Ahmed T. Youssef <xmonader@gmail.com>
2018-09-27 14:58:12 +0200

    initial work on triggering zrobot reconfigure

--- templates/zrobot_config/zrobot_config.py
+++ templates/zrobot_config/zrobot_config.py
@@ -5,6 +5,7 @@ from jumpscale import j
 
 from zerorobot.template.base import TemplateBase
 from zerorobot.config.data_repo import _parse_zdb
+from zerorobot import config
 
 CONFIG_PATH = '/opt/var/data/zrobot/zrobot_data/data_repo.yaml'
 
@@ -28,19 +29,30 @@ class ZrobotConfig(TemplateBase):
         install writes the config, then send a signal to the robot to stop itself
         in production 0-os will detect that the robot stops and will restart it
         """
-
+        if not j.sal.fs.exists(CONFIG_PATH):
+            j.sal.fs.touch(CONFIG_PATH)
         j.data.serializer.yaml.dump(
             CONFIG_PATH,
             {'zdb_url': self.data['dataRepo']}
         )
-        if j.core.state.configGetFromDict("myconfig", "backend") != "db":
-            (admin_password, hostname, port, namespace) = _parse_zdb(self.data['dataRepo'])
+        admin_password, hostname, port, namespace = _parse_zdb(self.data['dataRepo'])
+        if not port:
+            port = 9900
+        if not namespace:
+            namespace = "default"
+        if not admin_password:
+            admin_password = ""
 
+        if j.core.state.configGetFromDict("myconfig", "backend") == "db":
+            j.tools.configmanager.set_namespace(namespace)
+            # Robot should be running using a sandbox
+            j.tools.configmanager.configure_keys_from_sandbox(config.config_repo.path)
+        else:
+            j.core.state.configSetInDict("myconfig", "backend", "db")
             j.core.state.configSetInDict("myconfig", "backend_addr", "{}:{}".format(hostname, port))
             j.core.state.configSetInDict("myconfig", "adminsecret", admin_password)
-            secrets = j.core.state.configGetSetInDict("myconfig", "secrets", "")
-
-        self._kill_robot()
+            j.core.state.configSetInDict("myconfig", "secrets", "")
+            self._kill_robot()
 
     def delete(self):
         """
