a4a532ec008913367a6783e78e3cf4fb28ecb3e6
Ahmed T. Youssef <xmonader@gmail.com>
2018-10-08 13:40:25 +0200

    zrobot config (#135)
    * wrong password extraction in parse_zdb
    
    * initial work on triggering zrobot reconfigure
    
    * update zrobot_config to use repo key
    
    * delete method for template
    
    * update zrobot_config template to restart either way

--- templates/zrobot_config/zrobot_config.py
+++ templates/zrobot_config/zrobot_config.py
@@ -5,6 +5,7 @@ from jumpscale import j
 
 from zerorobot.template.base import TemplateBase
 from zerorobot.config.data_repo import _parse_zdb
+from zerorobot import config
 
 CONFIG_PATH = '/opt/var/data/zrobot/zrobot_data/data_repo.yaml'
 
@@ -28,11 +29,29 @@ class ZrobotConfig(TemplateBase):
         install writes the config, then send a signal to the robot to stop itself
         in production 0-os will detect that the robot stops and will restart it
         """
-
         j.data.serializer.yaml.dump(
             CONFIG_PATH,
             {'zdb_url': self.data['dataRepo']}
         )
+        admin_password, hostname, port, namespace = _parse_zdb(self.data['dataRepo'])
+        if not port:
+            port = 9900
+        if not namespace:
+            namespace = "default"
+        if not admin_password:
+            admin_password = ""
+
+        if j.core.state.configGetFromDict("myconfig", "backend") == "db":
+            j.tools.configmanager.set_namespace(namespace)
+            # Robot should be running using a sandbox
+            j.tools.configmanager.configure_keys_from_paths(config.config_repo.key, config.config_repo.key + ".pub")
+        else:
+            j.core.state.configSetInDict("myconfig", "backend", "db")
+            j.core.state.configSetInDict("myconfig", "backend_addr", "{}:{}".format(hostname, port))
+            j.core.state.configSetInDict("myconfig", "adminsecret", admin_password)
+            j.core.state.configSetInDict("myconfig", "secrets", "")
+            j.core.state.configSetInDict("myconfig", "namespace", namespace)
+
         self._kill_robot()
 
     def delete(self):
@@ -42,4 +61,6 @@ class ZrobotConfig(TemplateBase):
         if j.sal.fs.exists(CONFIG_PATH):
             j.sal.fs.remove(CONFIG_PATH)
 
+        # Should reset backend to file
+        j.core.state.configSetInDict("myconfig", "backend", "file")
         self._kill_robot()
