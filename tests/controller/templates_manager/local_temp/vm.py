e9ea41ee8031caf2a38f7b588366b9de8ab98cbe
islamTaha12 <islamtaha2012@gmail.com>
2018-10-15 15:17:13 +0200

    random ssh port

--- tests/controller/templates_manager/local_temp/vm.py
+++ tests/controller/templates_manager/local_temp/vm.py
@@ -1,7 +1,7 @@
 from jumpscale import j
 from zerorobot.service_collection import ServiceNotFoundError
 from testconfig import config
-
+import random
 
 class VMManager:
     def __init__(self, parent, service_name=None):
@@ -26,12 +26,13 @@ class VMManager:
             return self._vm_service
 
     def install(self, wait=True, **kwargs):
+        ssh_port = random.randint(22022, 22922)
         default_data = {
             'memory': 2048,
             'cpu': 1,
             'nics': [{'type': 'default', 'name': 'defaultnic'}],
             'flist': 'https://hub.grid.tf/tf-bootable/ubuntu:latest.flist',
-            'ports': [{'source': 22, 'target': 22, 'name': 'ssh'}],
+            'ports': [{'source': ssh_port, 'target': 22, 'name': 'ssh'}],
             'configs': [
                 {'path': '/root/.ssh/authorized_keys', 'content': config['vm']['ssh'],
                  'name': 'sshkey'}]
@@ -40,7 +41,7 @@ class VMManager:
             default_data.update(kwargs)
 
         self.vm_service_name = "vm_{}".format(self._parent._generate_random_string())
-        self.logger.info('Install {} vm'.format(self.vm_service_name))
+        self.logger.info('Install {} vm, ssh port : {} '.format(self.vm_service_name, ssh_port))
         self._vm_service = self.robot.services.create(self.vm_template, self.vm_service_name, default_data)
         self._vm_service.schedule_action('install').wait(die=wait)
 
