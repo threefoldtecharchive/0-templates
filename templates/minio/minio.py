45d700660861e912c0f65a79f6a5e337767dbe1e
Muhamad Azmy <muhamada@greenitglobe.com>
2018-10-30 13:14:11 +0200

    Minio Log processing (#188)
    * minio: preparation work for shards health miniotoring
    
    * minio: WIP monitor minio process logs to determin shards that are not
    reachable
    
    * formatting
    
    * Fix the message stats
    
    * WIP minio self healing
    
    * Fix tests
    
    * Fix tests

--- templates/minio/minio.py
+++ templates/minio/minio.py
@@ -1,9 +1,14 @@
-from jumpscale import j
+import time
+from json import JSONDecodeError
 
+from jumpscale import j
 from zerorobot.template.base import TemplateBase
 from zerorobot.template.state import StateCheckError
 from zerorobot.template.decorator import retry
 from zerorobot.service_collection import ServiceNotFoundError
+from zerorobot.template.state import (SERVICE_STATE_ERROR, SERVICE_STATE_OK,
+                                      SERVICE_STATE_SKIPPED,
+                                      SERVICE_STATE_WARNING, StateCheckError)
 
 PORT_MANAGER_TEMPLATE_UID = 'github.com/threefoldtech/0-templates/node_port_manager/0.0.1'
 
@@ -18,6 +23,7 @@ class Minio(TemplateBase):
     def __init__(self, name=None, guid=None, data=None):
         super().__init__(name=name, guid=guid, data=data)
         self._node_sal = j.clients.zos.get(NODE_CLIENT)
+        self._healer = Healer(self)
         self.add_delete_callback(self.uninstall)
         self.recurring_action('_monitor', 30)  # every 30 seconds
 
@@ -42,6 +48,8 @@ class Minio(TemplateBase):
                 self.state.set('status', 'running', 'ok')
         else:
             self.state.set('status', 'running', 'ok')
+
+        self._healer.start()
 
     @property
     def _minio_sal(self):
@@ -74,6 +82,10 @@ class Minio(TemplateBase):
         self.logger.info('Installing minio %s' % self.name)
         self._reserve_port()
         self.state.set('actions', 'install', 'ok')
+        for addr in self.data['zerodbs']:
+            self.state.set('data_shards', addr, SERVICE_STATE_OK)
+        if self.data['tlog']:
+            self.state.set('tlog_shards', self.data['tlog']['address'], SERVICE_STATE_OK)
 
     def start(self):
         """
@@ -83,6 +95,7 @@ class Minio(TemplateBase):
         self.logger.info('Starting minio %s' % self.name)
         minio_sal = self._minio_sal
         minio_sal.start()
+        self._healer.start()
         self.state.set('actions', 'start', 'ok')
         self.state.set('status', 'running', 'ok')
 
@@ -93,11 +106,13 @@ class Minio(TemplateBase):
         self.state.check('actions', 'install', 'ok')
         self.logger.info('Stopping minio %s' % self.name)
         self._minio_sal.stop()
+        self._healer.stop()
         self.state.delete('actions', 'start')
         self.state.delete('status', 'running')
 
     def uninstall(self):
         self.logger.info('Uninstalling minio %s' % self.name)
+        self.healer.stop()
         self._minio_sal.destroy()
 
         self._release_port()
@@ -113,6 +128,10 @@ class Minio(TemplateBase):
             minio_sal.create_config()
             minio_sal.reload()
 
+        # we consider shards info to be valid when we update them
+        for addr in self.data['zerodbs']:
+            self.state.set('data_shards', addr, SERVICE_STATE_OK)
+
     def update_tlog(self, namespace, address):
         self.data['tlog'] = {
             'namespace': namespace,
@@ -123,6 +142,10 @@ class Minio(TemplateBase):
         if minio_sal.is_running():
             minio_sal.create_config()
             minio_sal.reload()
+
+        # we consider shards info to be valid when we update them
+        if self.data['tlog']:
+            self.state.set('tlog_shards', self.data['tlog']['address'], SERVICE_STATE_OK)
 
     def update_master(self, namespace, address):
         self.data['master'] = {
@@ -146,3 +169,66 @@ class Minio(TemplateBase):
             return
         port_mgr = self.api.services.get(template_uid=PORT_MANAGER_TEMPLATE_UID, name='_port_manager')
         port_mgr.schedule_action("release", {"service_guid": self.guid, 'ports': [self.data['nodePort']]})
+
+
+LOG_LVL_STDOUT = 1
+LOG_LVL_STDERR = 2
+LOG_LVL_MESSAGE_PUBLIC = 3
+LOG_LVL_MESSAGE_INTERNAL = 4
+LOG_LVL_LOG_UNKNOWN = 5
+LOG_LVL_LOG_STRUCTURED = 6
+LOG_LVL_WARNING = 7
+LOG_LVL_OPS_ERROR = 8
+LOG_LVL_CRITICAL_ERROR = 9
+LOG_LVL_STATISTICS = 10
+LOG_LVL_RESULT_JSON = 20
+LOG_LVL_RESULT_YAML = 21
+LOG_LVL_RESULT_TOML = 22
+LOG_LVL_RESULT_HRD = 23
+LOG_LVL_JOB = 30
+
+
+class Healer:
+    MinioStreamKey = "minio.logs"
+
+    def __init__(self, minio):
+        self.service = minio
+        self.logger = minio.logger
+
+    def start(self):
+        self.logger.info("start minio logs processing")
+        if self.service.gl_mgr.gls.get(Healer.MinioStreamKey, None) is None:
+            self.service.gl_mgr.add(Healer.MinioStreamKey, self._process_logs)
+
+    def stop(self):
+        self.logger.info("stop minio logs processing")
+        self.service.gl_mgr.stop(Healer.MinioStreamKey, wait=True, timeout=5)
+
+    def _process_logs(self):
+        self.logger.info("processing logs for minio '%s'" % self.service)
+
+        def callback(level, msg, flag):
+            _health_monitoring(self.service.state, level, msg, flag)
+
+        while True:
+            # wait for the process to be running before processing the logs
+            try:
+                self.service.state.check('status', 'running', 'ok')
+            except StateCheckError:
+                time.sleep(5)
+                continue
+
+            # once the process is started, start monitoring the logs
+            # this will block until the process stops streaming (usually that means the process has stopped)
+            self.logger.info("calling minio stream method")
+            self.service._minio_sal.stream(callback)
+
+
+def _health_monitoring(state, level, msg, flag):
+    if level in [LOG_LVL_MESSAGE_INTERNAL]:
+        msg = j.data.serializer.json.loads(msg)
+        if 'shard' in msg:
+            state.set('data_shards', msg['shard'], SERVICE_STATE_ERROR)
+        if 'tlog' in msg and not msg.get('master', False):  # we check only the minio owns tlog server, not it's master
+            state.set('tlog_shards', msg['tlog'], SERVICE_STATE_ERROR)
+
