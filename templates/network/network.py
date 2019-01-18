290eede29e3c66bf72dd318a82650ed6f67d3e27
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2019-01-18 13:33:42 +0100

    network: improve alerting

--- templates/network/network.py
+++ templates/network/network.py
@@ -55,27 +55,67 @@ class Network(TemplateBase):
         node_id = self._node_sal.name
         data = {
             'attributes': {},
-            'resource': hostname,
             'environment': 'Production',
-            'severity': 'critical',
+            'severity': 'warning',
             'event': 'Network',
             'tags': ["node:%s" % hostname, "node_id:%s" % node_id],
             'service': [self.template_uid.name]
         }
 
         nics_by_name = {n['name']: n for n in self._node_sal.client.info.nic()}
+        nics_status = {name: {'healthy': True} for name in self.data['usedInterfaces']}
+
         for nic_name in self.data['usedInterfaces']:
+            alert_data = data.copy()
+            alert_data['resource'] = "%s-%s" % (hostname, nic_name)
+            alert_data['severity'] = 'critical' if nic_name == 'backplane' else 'warning'
+            alert_data['tags'].append('interface:%s' % nic_name)
+
             if nic_name not in nics_by_name:
-                data['text'] = "interface %s not found" % nic_name
-                send_alert(self.api.services.find(template_uid=ALERTA_UID), data)
+                nics_status[nic_name]['healthy'] = False
+                alert_data['text'] = "interface %s not found" % nic_name
+                send_alert(self.api.services.find(template_uid=ALERTA_UID), alert_data)
+
             elif 'up' not in nics_by_name[nic_name]['flags']:
-                data['text'] = "interface %s is down" % nic_name
-                data['tags'].append('interface:%s' % nic_name)
-                send_alert(self.api.services.find(template_uid=ALERTA_UID), data)
+                nics_status[nic_name]['healthy'] = False
+                alert_data['text'] = "interface %s is down" % nic_name
+                send_alert(self.api.services.find(template_uid=ALERTA_UID), alert_data)
+
             elif 'carrier' in nics_by_name[nic_name] and nics_by_name[nic_name]['carrier'] is False:
-                data['text'] = "interface %s has no carrier" % nic_name
-                data['tags'].append('interface:%s' % nic_name)
-                send_alert(self.api.services.find(template_uid=ALERTA_UID), data)
+                alert_data['text'] = "interface %s has no carrier" % nic_name
+                nics_status[nic_name]['healthy'] = False
+                send_alert(self.api.services.find(template_uid=ALERTA_UID), alert_data)
+
+        if 'backplane' in nics_status and nics_status['backplane'] is False:
+            alert_data = data.copy()
+            alert_data['severity'] = 'critical'
+            alert_data['text'] = 'backplane is down. Storage network is now offline for this node. Need imediate intervention'
+            alert_data['tags'].append('interface:backplane')
+            send_alert(self.api.services.find(template_uid=ALERTA_UID), alert_data)
+            return
+
+        if 'backplane' in nics_status:
+            del nics_status['backplane']
+
+        alert_data = data.copy()
+        alert_data['resource'] = "%s network" % (hostname)
+        nr_nic_down = [status['healthy'] for status in nics_status.values()].count(False)
+        self.logger.info("nr_nic_down %s", nr_nic_down)
+        self.logger.info("nics_status %s", nics_status)
+        if nr_nic_down == 0:
+            return
+
+        if nr_nic_down < len(nics_status):
+            alert_data['severity'] = 'major'
+            alert_data['text'] = 'Some interfaces used for the storage network are down. But network is still working'
+        elif nr_nic_down >= len(nics_status):
+            alert_data['severity'] = 'critical'
+            alert_data['text'] = 'All interfaces used for the storage network are down. Storage network is now offline for this node, needs imediate intervention'
+
+        for nic_name, status in nics_status.items():
+            if not status['healthy']:
+                alert_data['tags'].append('interface:%s' % nic_name)
+        send_alert(self.api.services.find(template_uid=ALERTA_UID), alert_data)
 
     def configure(self):
         self.logger.info('installing network %s' % self.name)
